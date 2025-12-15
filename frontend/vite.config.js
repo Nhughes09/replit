354e839b61168305696277526c7d5326328559f3
Nhughes09 <165423355+Nhughes09@users.noreply.github.com>
2025-12-15 13:33:14 -0500

    Fix Cloudflare build script & Add Logging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// Force Rebuild v2.0
+// Force Rebuild v11 - NEW GITHUB WAS USED
+console.log("NEW GITHUB WAS USED: Vite Config Loaded");
 export default defineConfig({
     plugins: [react()],
     build: {
